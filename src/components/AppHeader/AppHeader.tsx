44e1b15ac824a4257dff2f5ce7c16e6fa76b904d
Dor <dorhakim100@gmail.com>
2025-10-30 18:49:22 +0200

    Add basic header styling, several svgs

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -9,6 +9,9 @@ import { RootState } from '../../store/store'
 // import { DropdownMenu } from '../DropdownMenu/DropdownMenu'
 
 import logoImg from '../../../public/logo.png'
+import darkMode from '../../../public/icons/dark-mode.svg'
+import search from '../../../public/icons/search.svg'
+import profile from '../../../public/icons/profile.svg'
 
 interface AppHeaderProps {
   routes: Route[]
@@ -38,15 +41,28 @@ export function AppHeader({ routes }: AppHeaderProps) {
       >
         <img src={logoImg} alt='logo' className='logo-img' />
 
-        <nav>
-          <ul>
-            {routes.map((route, index) => (
-              <li key={index} onClick={() => navigateToPage(route.path)}>
-                <Link to={route.path}>{route.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <div className='navigation-container'>
+          <nav>
+            <ul>
+              {routes.map((route, index) => (
+                <li key={index} onClick={() => navigateToPage(route.path)}>
+                  <Link className='bold' to={route.path}>
+                    {route.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className='settings-container'>
+            <img src={darkMode} alt='dark mode' className='icon' />
+            <img src={search} alt='search' className='icon' />
+          </div>
+        </div>
+
+        <div className='profile-container'>
+          <img src={profile} alt='profile' className='icon profile-icon' />
+          <span className='bold'>כניסה לאזור האישי</span>
+        </div>
       </header>
     </>
   )
