c555f26670932a11d71cf93db394f21a2f038eec
Dor <dorhakim100@gmail.com>
2025-11-02 20:07:31 +0200

    Building project

--- src/components/PlayButton/PlayButton.tsx
+++ src/components/PlayButton/PlayButton.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline'
 import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline'
 
--- src/components/ShadowOverlay/ShadowOverlay.tsx
+++ src/components/ShadowOverlay/ShadowOverlay.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 interface ShadowOverlayProps {
   isVisble: boolean
   handleClose: () => void
